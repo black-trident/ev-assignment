804e780d75bc3ee3531adc084ec2179094e369f2
rishiks <rishiksun101@gmail.com>
2024-02-13 20:23:30 +0530

    setting up user display component as per requirement

--- fetch-user-app/src/components/UserDisplay.js
+++ fetch-user-app/src/components/UserDisplay.js
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 const UserDisplay = () => {
+    const [userData, setUserData] = useState({})
 
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('userData'));
+        if(storedUser){
+            setUserData(storedUser)
+        }else{
+            getUserData()
+        }     
+    },[])
 
-    return(
-        <div>
-            UserDisplay Comp
-        </div>
+    const getUserData = async () => {
+        try{
+            const response = await axios.get('https://randomuser.me/api')
+            console.log(response.data.results[0])
+            const {name,email} = response.data.results[0]
+            setUserData({name:`${name.title} ${name.first} ${name.last}`,email})
+            localStorage.setItem('userData', JSON.stringify({name:`${name.title} ${name.first} ${name.last}`,email}))
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    const handleRefreshButton = () => {
+        setUserData({})
+        getUserData()
+    }
+
+    return(   
+            (userData.name && userData.email) ? (
+                <div>
+                    <h3>User Details</h3>
+                    <ul>
+                        <li>Name: {userData.name}</li>
+                        <li>Email: {userData.email}</li>
+                    </ul>
+                    <button onClick={handleRefreshButton}>
+                        Refresh
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <h3>Fetching User Data ...</h3>
+                </div>
+            )       
     )
 }
 
